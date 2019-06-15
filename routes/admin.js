b49cd58d40b493e9ecbe2de3da9a5cfc43921189
jonribeiro23 <44584409+jonribeiro23@users.noreply.github.com>
2019-06-15 10:19:20 -0300

    Categorias

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.render('admin/index')
-})
+router.get('/', (req, res) => res.render('admin/index'))
 
-router.get('/posts', (req, res) => {
-  res.send('<button class="btn btn-danger">página de posts</button>')
-})
+router.get('/posts', (req, res) => res.send('<button class="btn btn-danger">página de posts</button>'))
 
-router.get('/categorias', (req, res) => {
-  res.send('página de categorias')
-})
+router.get('/categorias', (req, res) => res.render('admin/categorias'))
+
+router.get('/categorias/add', (req, res) => res.render('admin/add-categorias'))
 
 module.exports = router
